a829cd34e236093f707a5221dfb9cc70e8353449
RishabhXD <rishabhpant14@gmail.com>
2023-01-01 07:37:33 +0530

    sec commit

--- components/Buy.jsx
+++ components/Buy.jsx
@@ -14,20 +14,20 @@ const Buy = ({ state }) => {
   return (
     <div className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-[#171515] font-mono">
       <form onSubmit={buyCoffee} className="flex flex-col gap-6 mt-4 ">
-        <label className="label" htmlFor="name">
+        <label className="text-gray-700 dark:text-gray-200" htmlFor="name">
           Name
         </label>
         <input
-          className="input"
+          className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-[#171515] dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
           type="text"
           placeholder="Enter your name"
           id="name"
         />
-        <label className="label" htmlFor="message">
+        <label className="text-gray-700 dark:text-gray-200" htmlFor="message">
           Message
         </label>
         <input
-          className="input"
+          className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-[#171515] dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
           type="text"
           placeholder="Enter your message"
           id="message"
